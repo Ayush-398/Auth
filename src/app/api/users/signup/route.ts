f714e7da8858aea550875ba87f0e85b8b31264d1
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-13 19:07:42 +0530

    Login and Setup route completed

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,68 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel.js";
+import bctyptjs from  "bcryptjs";
+import { NextRequest,NextResponse } from "next/server";
+
+
+
+
+ 
+
+
+
+
+
+
+
+connect()
+
+
+export async function POST(request: NextRequest){
+    try{
+
+           const reqBody = await request.json()
+           const {username,email,password} = reqBody
+
+           console.log(reqBody);
+           const  user = await User.findOne({email})
+
+           if(user){
+            return NextResponse.json({error:"User already exists"},{status:400})
+           }
+
+
+
+
+
+           //hashing 
+
+           const salt = await bctyptjs.genSalt(10)
+           const hashedPassword = await bctyptjs.hash
+           (password,salt)
+
+           const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+           })
+
+
+           const savedUser = await newUser.save()
+           console.log(savedUser);
+
+          return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+          })
+
+
+
+
+
+
+    } catch(error:any){
+    return NextResponse.json({error:error.message},
+        {status:500}
+    )}
+}
