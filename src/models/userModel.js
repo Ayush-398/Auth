56170fc7be0e59d5805a082d235d6c47bad3aa27
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-14 12:53:35 +0530

    Updated forgotpassword route

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please provide a password"]
     },
-    isVerified: { // Fixed spelling
+    isVerified: { 
         type: Boolean,
         default: false,
     },
